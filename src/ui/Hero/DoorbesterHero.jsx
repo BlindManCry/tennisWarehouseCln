be5a9c7d57e082ca207c672691a0061e0e176be6
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-03-10 17:43:35 +0100

    dadw

---
+++ src/ui/Hero/DoorbesterHero.jsx
@@ -0,0 +1,17 @@
+function DoorbesterHero() {
+  return (
+    <div className="mt-[30px]">
+      <div className="bg-red-700">
+        <div>
+          <h2 className="text-center text-[40px] text-[#fff]">
+            Doorbuster Deal
+          </h2>
+          <p className="text-center text-[#fff]">Head Speed Team L</p>
+        </div>
+        <p>QRISTI</p>
+      </div>
+    </div>
+  );
+}
+
+export default DoorbesterHero;
