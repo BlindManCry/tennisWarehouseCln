d122255ff3dcb21cd7744470d22821f04b3014a5
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-03-10 20:57:07 +0100

    davamatet recquets komponenti

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../../ui/Hero/Hero";
 import NewArrivalsHero from "../../ui/Hero/NewArrivalsHero";
+import Racquets from "../../ui/Hero/Racquets/Racquets";
 
 function Home() {
   return (
     <div>
       <Hero />
       <NewArrivalsHero />
+      <Racquets />
     </div>
   );
 }
---
+++ src/ui/Hero/Racquets/Racquets.jsx
@@ -0,0 +1,70 @@
+function Racquets() {
+  return (
+    <div className="mt-[55px] mb-10">
+      <h2 className="text-[2rem] font-semibold text-center">
+        New 2024 Racquets
+      </h2>
+      <div className="flex mt-4">
+        <div className="w-[25%]">
+          <div className="border-[1px] border-gray-400 py-6 hover:border-black">
+            <img
+              src="https://img.tennis-warehouse.com/fpcache/224/custom_thumbnails/LEGACYRACT.jpg"
+              alt=""
+              className="ml-[50%] translate-x-[-50%]"
+            />
+            <div className="text-center mt-4">
+              <h2 className="text-[1.125rem] font-bold">Prince Legacy 2024</h2>
+              <p className="shop text-[0.75rem] font-bold underline">
+                Shop Now
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-[25%]">
+          <div className="border-[1px] border-gray-400 py-6 hover:border-black">
+            <img
+              src="https://img.tennis-warehouse.com/fpcache/224/custom_thumbnails/STRIKECT1.jpg"
+              alt=""
+              className="ml-[50%] translate-x-[-50%]"
+            />
+            <div className="text-center mt-4">
+              <h2 className="text-[1.125rem] font-bold">Prince Legacy 2024</h2>
+              <p className="text-[0.75rem] font-bold underline">Shop Now</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-[25%]">
+          <div className="border-[1px] border-gray-400 py-6 hover:border-black">
+            <img
+              src="https://img.tennis-warehouse.com/fpcache/224/custom_thumbnails/SPEED24CT.jpg"
+              alt=""
+              className="ml-[50%] translate-x-[-50%]"
+            />
+            <div className="text-center mt-4">
+              <h2 className="text-[1.125rem] font-bold">Prince Legacy 2024</h2>
+              <p className="text-[0.75rem] font-bold underline">Shop Now</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-[25%]">
+          <div className="border-[1px] border-gray-400 py-6 hover:border-black">
+            <img
+              src="https://img.tennis-warehouse.com/fpcache/224/custom_thumbnails/EZAQUACT.jpg"
+              alt=""
+              className="ml-[50%] translate-x-[-50%]"
+            />
+            <div className="text-center mt-4">
+              <h2 className="text-[1.125rem] font-bold">Prince Legacy 2024</h2>
+              <p className="text-[0.75rem] font-bold underline">Shop Now</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Racquets;
