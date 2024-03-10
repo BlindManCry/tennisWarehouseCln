47176c2288665d730ed5805a29fa225a243d26f7
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-03-10 12:58:26 +0100

    make switchGamesHero almost done

--- src/ui/Hero/SwitchImagesHero.jsx
+++ src/ui/Hero/SwitchImagesHero.jsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
+
 function SwitchImagesHero() {
+  const [activeImage, setActiveImage] = useState(0);
+  const [pause, setPause] = useState(false);
+
+  useEffect(
+    function () {
+      if (activeImage > 2) setActiveImage(0);
+      const imageInterval = setInterval(() => {
+        setActiveImage((img) => img + 1);
+      }, 4000);
+
+      return clearInterval(imageInterval);
+    },
+    [activeImage]
+  );
+
   return (
     <>
-      <div className="flex mt-7">
-        <div className="relative cursor-pointer">
+      <div className="flex mt-7 overflow-hidden">
+        {/* FIRST IMAGE  */}
+        <div
+          className={`relative cursor-pointer w-full min-w-full translate-x-[-${activeImage}00%] transition-all duration-300 `}
+        >
           <img
             src="https://img.tennis-warehouse.com/fpcache/1200/marketing/BOOM24FP3-lg.jpg"
             alt=""
@@ -20,9 +40,121 @@ function SwitchImagesHero() {
             </button>
           </div>
         </div>
+
+        {/* SECOND IMAGE  */}
+        <div
+          className={`relative cursor-pointer w-full min-w-full translate-x-[-${activeImage}00%] transition-all duration-300 `}
+        >
+          <img
+            src="https://img.tennis-warehouse.com/fpcache/1200/marketing/ADIMARKFP2-lg.jpg"
+            alt=""
+            className="w-full"
+          />
+          <div className="absolute left-3 w-[80px] h-[80px] top-[-230px]">
+            <img
+              src="https://img.tennis-warehouse.com/graphics-resizer/logos/ADIDAS.svg"
+              alt=""
+              className=" w-[80px] "
+            />
+          </div>
+          <div className="absolute bottom-9 left-9 ">
+            <h2 className="text-[1.5rem] leading-7 font-bold text-[#fff]">
+              New Markdowns
+            </h2>
+            <h2 className="text-[4rem] font-bold text-[#fff]">25% Off</h2>
+            <p className="text-[1.5rem] text-[#fff]">
+              adidas Melbourne Collection
+            </p>
+            <button className="text-[#fff] px-5 py-3 bg-[#b70510] font-semibold rounded-sm mt-4">
+              Shop Now
+            </button>
+          </div>
+        </div>
+
+        {/* THIRD IMAGE  */}
+        <div
+          className={`relative cursor-pointer w-full min-w-full translate-x-[-${activeImage}00%] transition-all duration-300 `}
+        >
+          <img
+            src="https://img.tennis-warehouse.com/fpcache/1200/marketing/241RPMFP1-lg.jpg"
+            alt=""
+            className="w-full"
+          />
+          <div className="absolute left-3 w-[80px] h-[80px] top-[-230px]">
+            <img
+              src="https://img.tennis-warehouse.com/graphics-resizer/logos/BABOLAT.svg"
+              alt=""
+              className=" w-[80px] "
+            />
+          </div>
+          <div className="absolute bottom-9 left-9 ">
+            <h2 className="text-[1.5rem] leading-7 font-bold text-[#fff]">
+              Babolat String Stale
+            </h2>
+            <h2 className="text-[4rem] font-bold text-[#fff]">
+              Buy 2, Get 1 Free
+            </h2>
+            <p className="text-[1.5rem] text-[#fff]">RPM String Sets & Reels</p>
+            <button className="text-[black] px-5 py-3 bg-[#fff] font-semibold rounded-sm mt-4">
+              Shop Now
+            </button>
+          </div>
+        </div>
       </div>
 
-      <div></div>
+      <div className="flex items-center">
+        <p
+          className="w-[32%] text-center  py-4 border-x-[1px] border-b-[1px] border-gray-300 cursor-pointer"
+          onClick={() => setActiveImage(0)}
+        >
+          Head Boom 2024
+        </p>
+        <p
+          className="w-[32%] text-center py-4  border-x-[1px] border-b-[1px] border-gray-300 cursor-pointer"
+          onClick={() => setActiveImage(1)}
+        >
+          New Markdowns 25% Off
+        </p>
+        <p
+          className="w-[32%] text-center py-4  border-x-[1px] border-b-[1px] border-gray-300 cursor-pointer"
+          onClick={() => setActiveImage(2)}
+        >
+          Babolat String Sale Buy 2, Get 1 Free
+        </p>
+        {pause ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-[4%] h-[55px] object-cover cursor-pointer self-center  border-x-[1px] border-b-[1px] border-gray-300"
+            onClick={() => setPause(false)}
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-[4%] h-[55px] object-cover cursor-pointer self-center  border-x-[1px] border-b-[1px] border-gray-300"
+            onClick={() => setPause(true)}
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 5.25v13.5m-7.5-13.5v13.5"
+            />
+          </svg>
+        )}
+      </div>
     </>
   );
 }
