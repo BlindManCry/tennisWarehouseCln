f78105726113bbcc637ac5c8e267a164d1ce0c47
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-03-11 10:20:27 +0100

    fix data update

--- src/components/singleItemComponents/NewArrivalItem.jsx
+++ src/components/singleItemComponents/NewArrivalItem.jsx
@@ -1,10 +1,17 @@
-function NewArrivalItem({ item }) {
+import {
+  wrap_classnameHeight,
+  wrap_classnameWidth,
+} from "../../helpers/classname";
+
+function NewArrivalItem({ item, index }) {
   return (
     <div className="flex flex-col justify-center items-left min-w-[25%] border-2 h-[350px]">
       <img
         src={item.image}
         alt="item"
-        className={`w-[${item.imageWidth}px] h-[${item.imageHeight}px] mx-[50%] translate-x-[-50%]`}
+        className={`${wrap_classnameWidth(index)}
+        ${wrap_classnameHeight(index)}
+        mx-[50%] translate-x-[-50%]`}
       />
       <p>New</p>
       <p>{item.title}</p>
---
+++ src/helpers/classname.js
@@ -0,0 +1,67 @@
+export function wrap_classnameWidth(index) {
+  return index === 0
+    ? "w-[210px]"
+    : index === 1
+    ? "w-[120px]"
+    : index === 2
+    ? "w-[210px]"
+    : index === 3
+    ? "w-[120px]"
+    : index === 4
+    ? "w-[210px]"
+    : index === 5
+    ? "w-[210px]"
+    : index === 6
+    ? "w-[210px]"
+    : index === 7
+    ? "w-[210px]"
+    : index === 8
+    ? "w-[210px]"
+    : index === 9
+    ? "w-[210px]"
+    : index === 10
+    ? "w-[210px]"
+    : index === 11
+    ? "w-[210px]"
+    : index === 12
+    ? "w-[210px]"
+    : index === 13
+    ? "w-[210px]"
+    : index === 14
+    ? "w-[210px]"
+    : "";
+}
+
+export function wrap_classnameHeight(index) {
+  return index === 0
+    ? "h-[157px]"
+    : index === 1
+    ? "h-[278px]"
+    : index === 2
+    ? "h-[157px]"
+    : index === 3
+    ? "h-[278px]"
+    : index === 4
+    ? "h-[157px]"
+    : index === 5
+    ? "h-[280px]"
+    : index === 6
+    ? "h-[280px]"
+    : index === 7
+    ? "h-[157px]"
+    : index === 8
+    ? "h-[280px]"
+    : index === 9
+    ? "h-[280px]"
+    : index === 10
+    ? "h-[280px]"
+    : index === 11
+    ? "h-[280px]"
+    : index === 12
+    ? "h-[280px]"
+    : index === 13
+    ? "h-[280px]"
+    : index === 14
+    ? "h-[280px]"
+    : "";
+}
