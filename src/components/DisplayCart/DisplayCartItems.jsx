e598d7faeb61d0e6bdf887685be7982c40048061
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-03-12 17:17:25 +0100

    add cart component with hard codinig

--- src/components/DisplayCart/DisplayCartItems.jsx
+++ src/components/DisplayCart/DisplayCartItems.jsx
@@ -1,5 +1,33 @@
 function DisplayCartItems() {
-  return <div></div>;
+  return (
+    <>
+      <div className="mt-6 px-9 flex">
+        <div className="flex w-[69%] min-w-[55%]">
+          <img
+            src="https://img.tennis-warehouse.com/watermark/rs.php?path=TWHC-BL-1.jpg&nw=128 "
+            alt=""
+            className="w-[100px] h-[134px]"
+          />
+          <div className="w-[100%] flex flex-col gap-7 ml-9">
+            <p className="font-bold">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+            <p className="text-[0.75rem] text-[#215ad0] cursor-pointer hover:underline">
+              Remove
+            </p>
+          </div>
+        </div>
+
+        <div className="ml-5 w-[50%] flex justify-between">
+          <div className="flex gap-[90px]">
+            <p className="text-[#007A62]">Yes</p>
+            <p>1</p>
+          </div>
+          <p className="mr-[30px]">$25.00</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default DisplayCartItems;
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -15,10 +15,22 @@ function Cart() {
           <button className="bg-gray-200 text-gray-600 font-semibold py-2 px-[140px]">
             Checkout
           </button>
+          {/* <button className="bg-blue-200 text-blue-600 font-semibold py-2 px-[140px]">
+            Checkout
+          </button> */}
         </div>
       </div>
 
-      <div></div>
+      <div className="flex justify-between items-center mt-6">
+        <p className="font-bold ml-[130px] text-[0.75rem]">ITEM</p>
+        <div className="flex gap-[130px]">
+          <div className="flex gap-11">
+            <p className="font-bold text-[0.75rem]">IN STOCK</p>
+            <p className="font-bold text-[0.75rem]">QUANTITY</p>
+          </div>
+          <p className="mr-[70px] font-bold text-[0.75rem]">PRICE</p>
+        </div>
+      </div>
 
       <DisplayCartItems />
     </div>
