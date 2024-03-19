33404403e1cc44c539d149a42dbfa9c02193e951
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-03-19 16:06:33 +0100

    sign up logic

--- src/ui/ModalSignUp.jsx
+++ src/ui/ModalSignUp.jsx
@@ -8,10 +8,27 @@ function ModalSignUp() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [signIn, setSignIn] = useState(false);
+  const [isNotMatched, setIsNotMatched] = useState(false);
+  const [smallPassword, setSmallPassword] = useState(false);
+  console.log(smallPassword);
   const { setIsOpen, usersData } = useItems();
 
   async function handleSubmit(e) {
     e.preventDefault();
+
+    if (!username || !email || !password || !confirmPassword) return;
+
+    if (password.length < 8) {
+      setSmallPassword(true);
+      setIsNotMatched(false);
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setIsNotMatched(true);
+      setSmallPassword(false);
+      return;
+    }
 
     const newObj = {
       id: crypto.randomUUID(),
@@ -26,6 +43,7 @@ function ModalSignUp() {
       body: JSON.stringify(newObj),
     });
     console.log(usersData);
+    setIsOpen(false);
   }
 
   if (signIn) return <Modal />;
@@ -34,7 +52,7 @@ function ModalSignUp() {
     <div className="fixed top-0 right-0 left-0 bottom-0 max-h-[100vh] z-10 backdrop-blur-sm">
       <form
         onSubmit={handleSubmit}
-        className=" w-[500px] h-[550px] bg-white absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  z-50 rounded-md"
+        className=" w-[500px] h-auto pb-9 bg-white absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  z-50 rounded-md"
       >
         <div className="flex justify-between w-[90%] mx-auto mt-4 mb-2 items-center">
           <h1 className="text-[24px] font-bold">Account Login</h1>
@@ -85,17 +103,30 @@ function ModalSignUp() {
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
-          <p className="absolute text-blue-500 text-[13px] hover:cursor-pointer hover:border-b-[1px] hover:border-blue-500 right-[25px] top-[118%]">
+
+          <p className="absolute text-blue-500 text-[13px] hover:cursor-pointer hover:border-b-[1px] hover:border-blue-500 right-[25px] top-[115%]">
             Forgot Password?
           </p>
         </div>
+
+        {smallPassword && (
+          <p className="text-start mt-4 ml-7 text-red-500 font-semibold text-[14px]">
+            Password must be at least 8 characters
+          </p>
+        )}
+
+        {isNotMatched && (
+          <p className="text-start mt-4 ml-7 text-red-500 font-semibold text-[14px]">
+            Passwords do not match
+          </p>
+        )}
 
         <div className="flex mt-[60px] justify-between w-[90%] mx-auto mb-[50px]">
           <div className="flex bg-gray-100 w-[214px] h-[53] gap-2 p-2 ">
             <input type="checkbox" />
             <p className="text-[18px]">Remember Me</p>
           </div>
-          <button className="px-[30px] bg-orange-500 text-[18px] w-[214px] h-[47px] hover:cursor-pointer ">
+          <button className="px-[30px] bg-orange-500 text-[18px] w-[214px] h-[47px] hover:cursor-pointer">
             Sign Up
           </button>
         </div>
